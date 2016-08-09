3df6ccbb086e76414f5b7b012c520e4b7d68b731
MISHIRO <mishiro1127@gmail.com>
2016-08-09 20:56:28 +0900

    2016/08/09/21:00

--- わからないことクラス.java
+++ わからないことクラス.java
@@ -1,17 +1,43 @@
 package CardGame;
 
 public class わからないことクラス {
-/* ????????????????????????????????????????????????????
- * ?  なぜかplayerが表示されなくなった                ?
- * ?  g.drawImage(player, 75, 545, 150, 200, this);   ?
- * ?  と書いているので表示されるはずなのに...         ?
- * ?  g.drawImage(enemy, 1575, 255, 150, 200, this);  ?
- * ?  enemyの方はちゃんと表示されている               ?
- * ????????????????????????????????????????????????????
- * 
- *  if (needRepainty == true) {
- *  repaint();
- *  
- * なぜこれでは動かないんですか？
- * */
+	
+/*
+public static int doro(List<Card> d,int HP) {
+		
+		if(d.get(0)==null){
+			return HP=0;
+		}
+		d.get(0);
+		return HP;
+		
+	}
+	
+	これはどういうことなんですか?
+	
+	*/
+	
+	/*
+	 * public void cardpaint(Graphics g) {
+		g.setColor(Color.black);
+		Card[] playerField = new Card[5];
+		Card card = cd.findCardData("ドラゴンガード");
+		playerField[0] = card;
+		for (int i = 0; i < 5; i++) {
+			if (playerField[i] == null) {
+        //ココ、一番右だけ表示させる
+			} else {
+				g.drawRoundRect(1275 - 225 * i, 545, 150, 200, 10, 10);
+			}
+		}
+		for (int i = 0; i < 5; i++) {
+			g.drawRoundRect(1275 - 225 * i, 255, 150, 200, 10, 10);
+		}
+	}
+	
+	一番右側だけを表示させたい場合は普通に
+	g.drawRoundRect(1275 * i, 545, 150, 200, 10, 10);
+	で表示させればいいのですか？
+	1学期中の授業で何か違う書き方をしていた気がするのですが気のせいですか？
+	 * */
 }
